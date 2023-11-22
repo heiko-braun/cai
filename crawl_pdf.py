325f369c61db5b7b78b194e6b1bd3def866b86f2
heiko-braun <ike.braun@googlemail.com>
2023-11-22 11:03:21 +0100

    util for crawling pdfs

---
+++ crawl_pdf.py
@@ -0,0 +1,17 @@
+from langchain.document_loaders import PyPDFLoader
+from conf.constants import *
+
+loader = PyPDFLoader("./docs/red_hat_fuse-7.12-apache_camel_development_guide-en-us.pdf")
+pages = loader.load_and_split()
+COLLECTION_NAME="fuse_camel_development"
+
+offset = 31
+for page in pages[offset:]:
+    page_num = page.metadata["page"]
+    print("Parsing page ", page_num)
+    try:        
+        with open(TEXT_DIR+COLLECTION_NAME+'/page_'+str(page_num) + ".txt", "w", encoding="UTF-8") as f:                                
+            f.write(page.page_content)
+    except Exception as e:
+        print("Unable to parse page " + page_num)
+        print(e)
