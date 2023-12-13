3dde20c9a0a79e8bfe62d811c4febbaaa3497400
heiko-braun <ike.braun@googlemail.com>
2023-12-13 17:34:45 +0100

    Fix typo

--- agent.py
+++ agent.py
@@ -31,7 +31,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-"# Camel Quickstart Assitant"
+"# Camel Quickstart Assistant"
 
 def create_qdrant_client(): 
     client = QdrantClient(
